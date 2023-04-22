1069afca690bca508fad9d0ad6d6bb25e6f66578
Paul Figueroa <pjsfigueroa@gmail.com>
2023-04-22 23:21:19 +0800

    Functional create, update, delete, fetch user roles APIs.

--- models/Role.js
+++ models/Role.js
@@ -5,13 +5,17 @@ const RoleSchema = new mongoose.Schema(
     createdBy: String,
     name: {
       type: String,
+      required: true,
       max: 100
     },
     description: {
       type: String,
       max: 200
     },
-    permissions: Array
+    permissions: {
+      type: Array,
+      required: true
+    }
   },
   {
     timestamps: true
