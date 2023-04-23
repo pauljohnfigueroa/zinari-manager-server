179dbbb5699f2602b4d28931d8da2ed45c0ee447
Paul Figueroa <pjsfigueroa@gmail.com>
2023-04-23 23:29:27 +0800

    Refactor user model.

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,91 +3,89 @@ import jwt from 'jsonwebtoken'
 
 import User from '../models/User.js'
 
-
 export const register = async (req, res) => {
-    try {
-        // get the user information from the form using req.body
-        const {
-            firstName,
-            lastName,
-            extName,
-            email,
-            phone,
-            password,
-            password2,
-            roles,
-            photo,
-            departments,
-            branches,
-            teams,
-            projects,
-            tasks,
-            digitalSignature
-        } = req.body
+  try {
+    // get the user information from the form using req.body
+    const {
+      firstName,
+      lastName,
+      extName,
+      email,
+      phone,
+      password,
+      //   password2,
+      //   roles,
+      //   photo,
+      //   departments,
+      //   branches,
+      //   teams,
+      //   projects,
+      //   tasks,
+      //   digitalSignature,
+      createdBy
+    } = req.body
 
-        if (password !== password2) return { "error": "Password must match." }
+    //if (password !== password2) return { error: 'Password must match.' }
 
-        // password salt
-        const salt = await bcrypt.genSalt()
-        // password hash
-        const passwordHash = await bcrypt.hash(password, salt)
+    // password salt
+    const salt = await bcrypt.genSalt()
+    // password hash
+    const passwordHash = await bcrypt.hash(password, salt)
 
-        // new User()
-        const newUser = new User({
-            firstName,
-            lastName,
-            extName,
-            phone,
-            email,
-            password: passwordHash,
-            roles,
-            photo,
-            departments,
-            branches,
-            teams,
-            projects,
-            tasks,
-            digitalSignature
-        })
-        // save new user
-        const savedUser = await newUser.save()
+    // new User()
+    const newUser = new User({
+      firstName,
+      lastName,
+      extName,
+      email,
+      phone,
+      password: passwordHash,
+      //   roles,
+      //   photo,
+      //   departments,
+      //   branches,
+      //   teams,
+      //   projects,
+      //   tasks,
+      //   digitalSignature,
+      createdBy
+    })
+    // save new user
+    const savedUser = await newUser.save()
 
-        // response
-        // do not include password to the response
-        savedUser.password = undefined
-        res.status(201).json(savedUser)
-
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
+    // response
+    // do not include password to the response
+    savedUser.password = undefined
+    res.status(201).json(savedUser)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
 }
 
 export const login = async (req, res) => {
-    try {
-        // get the login parameters
-        const { email, password } = req.body
+  try {
+    // get the login parameters
+    const { email, password } = req.body
 
-        // check if the email exists in the database
-        const user = await User.findOne({ email })
+    // check if the email exists in the database
+    const user = await User.findOne({ email })
 
-        // return error if the email does not exists
-        if (!user) return res.status(400).json({ error: "Email does not exists." })
+    // return error if the email does not exists
+    if (!user) return res.status(400).json({ error: 'Email does not exists.' })
 
-        // If the email exists, check the password
-        const isPwdMatch = await bcrypt.compare(password, user.password)
-        // If password does not match
-        if (!isPwdMatch) return res.status(400).json({ error: "Invalid credentials." })
+    // If the email exists, check the password
+    const isPwdMatch = await bcrypt.compare(password, user.password)
+    // If password does not match
+    if (!isPwdMatch) return res.status(400).json({ error: 'Invalid credentials.' })
 
-        // if everything is OK
-        // do not send the password to the front end
-        user.password = undefined
-        // generate token
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
-        // send credentials to the front end
-        res.status(200).json({ user, token })
-
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
+    // if everything is OK
+    // do not send the password to the front end
+    user.password = undefined
+    // generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    // send credentials to the front end
+    res.status(200).json({ user, token })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
 }
-
--- models/User.js
+++ models/User.js
@@ -1,73 +1,64 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const UserSchema = new mongoose.Schema({
-
+const UserSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 50
+      type: String,
+      required: true,
+      min: 2,
+      max: 50
     },
     lastName: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 50
+      type: String,
+      required: true,
+      min: 2,
+      max: 50
     },
     extName: {
-        type: String,
-        min: 1,
-        max: 15
+      type: String,
+      min: 1,
+      max: 15
     },
     email: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 50
+      type: String,
+      required: true,
+      min: 2,
+      max: 50
     },
     phone: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     roles: {
-        type: Array,
-        required: true
+      type: Array,
+      required: false
     },
     photo: {
-        type: String,
-        required: true
+      type: String,
+      required: false
     },
     departments: {
-        type: Array,
-        required: true
+      type: Array,
+      required: false
     },
     branches: {
-        type: Array,
-        required: true
+      type: Array,
+      required: false
     },
-    teams: {
-        type: Array,
-        required: true
-    },
-    projects: {
-        type: Array,
-        required: true
-    },
-    tasks: {
-        type: Array,
-        required: true
-    },
-    digitalSignature: {
-        type: String,
-        required: true
-    }
-}, {
+    teams: Array,
+    projects: Array,
+    tasks: Array,
+    digitalSignature: String,
+    createdBy: String
+  },
+  {
     timestamps: true
-})
+  }
+)
 
 const User = mongoose.model('User', UserSchema)
 
