81d4e7de628b51e25c4b64e1a06097e186c394c5
Paul Figueroa <pjsfigueroa@gmail.com>
2023-06-07 17:19:05 +0800

    Create a Comment model for storing user comments.

--- models/Task.js
+++ models/Task.js
@@ -1,23 +1,25 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const TaskSchema = new mongoose.Schema({
+const TaskSchema = new mongoose.Schema(
+  {
     owner: String,
     title: {
-        type: String,
-        max: 100
+      type: String,
+      max: 100
     },
     description: {
-        type: String,
-        max: 200
+      type: String,
+      max: 200
     },
-    comments: Array,
     status: String,
     priority: String,
     category: String,
     dueDate: String
-}, {
+  },
+  {
     timestamps: true
-})
+  }
+)
 
 const Task = mongoose.model('Task', TaskSchema)
 export default Task
